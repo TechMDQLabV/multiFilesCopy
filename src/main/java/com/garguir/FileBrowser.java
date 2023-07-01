0529951a1cdb68db09e7dbd4234c7513edcc74a2
Sergio Garguir <sergiogarguir@gmail.com>
2023-07-01 11:07:39 -0300

    update - refactor and generate csv file

--- src/main/java/com/garguir/FileBrowser.java
+++ src/main/java/com/garguir/FileBrowser.java
@@ -1,9 +1,15 @@
 package com.garguir;
 
+import com.garguir.models.MockFile;
+
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class FileBrowser {
@@ -11,6 +17,7 @@ public class FileBrowser {
     private static final String USER_DIR = System.getProperty("user.dir");
     private static final String PATH_ORIGIN_FILES = USER_DIR+"\\src\\main\\resources";
     private static final String PATH_NEW_FILES = USER_DIR+"\\specs\\";
+    private static final String PATH_TXT = USER_DIR+"\\specs\\urls.csv";
     private static final String OK = "ok";
     private static final String REQUEST = "Request";
     private static final String RESPONSE = "Response";
@@ -19,25 +26,35 @@ public class FileBrowser {
     private static final String LINE_EQUALS =      "===================================================================================================";
     private static final String LINE_UNDERLINEDS = "___________________________________________________________________________________________________";
     private static final String LINE_MIDDLE =      "---------------------------------------------------------------------------------------------------";
-    static int nro = 0;
-    File filesRoute;
+    private static final String SEMICOLON = ";";
+    private static final String LINE_BREAK = "\n";
+    private static final String FALSE = "false";
+    private static final String FILE_HEADER = "File" + SEMICOLON + "From DIR" + SEMICOLON + "Request" + LINE_BREAK;
+    private static int nro = 0;
+    private final List<MockFile> mockFiles = new ArrayList<>();
 
-    public FileBrowser(String route) {
-        this.filesRoute = new File(route);
+    public FileBrowser() {
+
     }
 
-    public void listFiles(String path) throws IOException {
+    public void execute() throws IOException {
+        listFiles(PATH_ORIGIN_FILES);
+        saveUrls();
+    }
+
+    private void listFiles(String path) throws IOException {
         File file = new File(path);
         if (file.exists()) {
             File[] files = file.listFiles();
 
             if (files != null) {
-                for (int i = 0; i < files.length; i++) {
-                    if (files[i].isFile()) {
-                        createFiles(files[i]);
-                    } else if (files[i].isDirectory()) {
-                        createDir(files[i]);
-                        listFiles(files[i].getAbsolutePath());
+                for (File value : files) {
+                    if (value.isFile()) {
+                        mockFiles.add(new MockFile(value.getName(), value.getParent()));
+                        createFiles(value);
+                    } else if (value.isDirectory()) {
+                        createDir(value);
+                        listFiles(value.getAbsolutePath());
                     }
                 }
             }
@@ -46,25 +63,23 @@ public class FileBrowser {
         }
     }
 
-    private static String getNewPath(File file){
+    private String getNewPath(File file){
         return PATH_NEW_FILES + file.getParent()
                 .replace(PATH_ORIGIN_FILES, "")
                 .replace("\\prp", "")
                 .replace(" ", "_") + "\\";
     }
 
-    private static void createDir(File file){
+    private void createDir(File file){
         if(!file.getName().equals("prp")) {
             File newDir = new File( getNewPath(file) + file.getName().replace(" ","_"));
-            if (!newDir.exists()) {
-                if (!newDir.mkdir()) {
-                    LOGGER.warning("createDir >>>>>>>>>>>>>>>>>>>>>>>>>>> Error al crear Directorio " + newDir.getAbsolutePath());
-                }
+            if (!newDir.exists() && (!newDir.mkdir())) {
+                LOGGER.warning("createDir >>>>>>>>>>>>>>>>>>>>>>>>>>> Error al crear Directorio " + newDir.getAbsolutePath());
             }
         }
     }
 
-    private static String createNewDir(File file){
+    private String createNewDir(File file){
         //nro++;
         String fileName = getFileNameWithoutExtension(file.getName());
         String newDirName = (fileName.length() > 0 && !onlySpaces(fileName))?fileName.replace(" ", "_"):Integer.toString(++nro);
@@ -75,18 +90,16 @@ public class FileBrowser {
                 LOGGER.warning("createDir >>>>>>>>>>>>>>>>>>>>>>>>>>> Error al crear Directorio "+newFileNameDir.getAbsolutePath());
                 return null;
             }else {
-                if (!newOkDir.exists()) {
-                    if(!newOkDir.mkdir()){
-                        LOGGER.warning("createDir >>>>>>>>>>>>>>>>>>>>>>>>>>> Error al crear Directorio "+newOkDir.getAbsolutePath());
-                        return null;
-                    }
+                if (!newOkDir.exists() && (!newOkDir.mkdir())){
+                    LOGGER.warning("createDir >>>>>>>>>>>>>>>>>>>>>>>>>>> Error al crear Directorio "+newOkDir.getAbsolutePath());
+                    return null;
                 }
             }
         }
         return newOkDir.getAbsolutePath();
     }
 
-    private static String getFileExtension(String fileName){
+    private String getFileExtension(String fileName){
         int x = fileName.lastIndexOf(".");
         if(x!=-1){
             return fileName.substring(x);
@@ -95,11 +108,11 @@ public class FileBrowser {
         }
     }
 
-    private static String getFileNameWithoutExtension(String fileName){
+    private String getFileNameWithoutExtension(String fileName){
         return fileName.replace(getFileExtension(fileName),"").replace(" ", "_");
     }
 
-    private static boolean onlySpaces(String s){
+    private boolean onlySpaces(String s){
         for(int i=0; i<s.length();i++){
             if(s.charAt(i) != ' '){
                 return false;
@@ -119,6 +132,28 @@ public class FileBrowser {
             LOGGER.info("Copiando archivo a la nueva carpeta: " + newDestinyDir);
         } catch (IOException e) {
             LOGGER.warning("Error al escribir el archivo: "+e);
+        }
+    }
+
+    private void saveUrls(){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(FILE_HEADER);
+        try{
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(PATH_TXT));
+
+            for(MockFile mockFile : this.mockFiles){
+                stringBuilder.append(mockFile.getFileName());
+                stringBuilder.append(SEMICOLON);
+                stringBuilder.append(mockFile.getParentPath());
+                stringBuilder.append(SEMICOLON);
+                stringBuilder.append(FALSE);
+                stringBuilder.append(LINE_BREAK);
+            }
+            bufferedWriter.write(stringBuilder.toString());
+            bufferedWriter.close();
+            LOGGER.info("Archivo .csv guardado");
+        }catch (IOException ioException){
+            LOGGER.warning("Error: Fallo de escritura del archivo .csv " + ioException);
         }
     }
 }
--- src/main/java/com/garguir/Main.java
+++ src/main/java/com/garguir/Main.java
@@ -3,13 +3,11 @@ package com.garguir;
 import java.io.IOException;
 
 public class Main {
-    private static final String USER_DIR = System.getProperty("user.dir");
-    private static final String PATH_ORIGIN_FILES = USER_DIR+"\\src\\main\\resources";
 
     public static void main(String[] args) throws IOException {
         System.out.println("Hello FilesCopy!!!!!");
-        FileBrowser fileBrowser = new FileBrowser(PATH_ORIGIN_FILES);
+        FileBrowser fileBrowser = new FileBrowser();
 
-        fileBrowser.listFiles(PATH_ORIGIN_FILES);
+        fileBrowser.execute();
     }
 }
